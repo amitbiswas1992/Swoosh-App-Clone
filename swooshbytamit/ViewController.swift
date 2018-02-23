162e08e11875442dd2a848a0a8c6d9f19ab6cc02
Amit K Biswas <biswas1992@hotmail.com>
2018-02-23 16:18:23 -0500

    Initial Commit

---
+++ swooshbytamit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swooshbytamit
+//
+//  Created by MazeGeek on 2/23/18.
+//  Copyright © 2018 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
