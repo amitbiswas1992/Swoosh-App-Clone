8f6b4e78b1dc5153a9d6b830d240dd4033352e2b
Amit K Biswas <biswas1992@hotmail.com>
2018-02-28 17:30:00 -0500

    created model for player

--- swooshbytamit/Controller/LeagueVC.swift
+++ swooshbytamit/Controller/LeagueVC.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
     
+    
+    @IBOutlet weak var nextButton: BorderButton!
     
   
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       player = Player()
     }
 
     
@@ -30,6 +33,40 @@ class LeagueVC: UIViewController {
     @IBAction func unwindFromLeagueVC (unwindLeagueVCSegue : UIStoryboardSegue){
         
     }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+    
+     selectLeague(leagueType: "mens")
+        
+    }
+    
+    
+    @IBAction func onwomensTapped(_ sender: Any) {
+    
+        selectLeague(leagueType: "womens")
+        
+    }
+    
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+    
+     selectLeague(leagueType: "coed")
+    
+    }
+    
+    
+    func selectLeague(leagueType: String ){
+    player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    
+    }
+    
+    
+    
+    
+    
+    
+    
     
     /*
     // MARK: - Navigation
---
+++ swooshbytamit/Model/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  swooshbytamit
+//
+//  Created by MazeGeek on 2/28/18.
+//  Copyright © 2018 Amit Biswas. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String?
+    
+    var slectedSkillsLevel: String?
+    
+}
+
